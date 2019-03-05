ffe544913dc9d5caa204904870ea63f3c5e8c996
Umar Khan <umarkhandev@gmail.com>
2019-03-05 21:50:02 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.all? {|i| i.start_with? "r"}
+end
+
+def contain_a(arr)
+  arr.select do |x|
+    x.include?"a"
+  end
+end
+
+def first_wa(arr)
+ arr = arr.grep(String)
+ arr.find do |x|
+   x.start_with?"wa"
+ end
+end
+
+def remove_non_strings(arr)
+  arr.grep(String)
+end
+
+def count_elements(arr)
+  count = Hash.new(0)
+  arr.each {|x| count[x] += 1}
+  count.map { |key, value| ":count => #{value}, #{key}" }.join(', ')
+end
